c2a0d9f3054dada454598ea7236a55712f446a37
Kritsakorn Khamwiset <kritsakorn224@gmail.com>
2024-10-03 04:47:56 +0700

    Replacing MathJax with Katex due to production problems

--- Numer/src/Component/Elements/MathEquation.jsx
+++ Numer/src/Component/Elements/MathEquation.jsx
@@ -2,11 +2,9 @@ import { InlineMath } from 'react-katex';
 import 'katex/dist/katex.min.css'; 
 
 const MathEquation = ({ equation }) => {
-
-  const formattedEquation = equation;
   return (
     <div>
-      <InlineMath>{formattedEquation}</InlineMath>
+      <InlineMath>{equation}</InlineMath>
     </div>
   );
 };
--- Numer/src/Component/Elements/RootOfEquation.jsx
+++ Numer/src/Component/Elements/RootOfEquation.jsx
@@ -9,22 +9,22 @@ const  RootOfEquation = () => {
         <Link to="/Root_of_Equation/Graphical" className="s:w-1/4 md:w-1/4 lg:w-1/4 s:h-1/4 md:h-1/3 lg:h-1/4 border border-slate-300 
             rounded-lg m-5 no-underline text-black hover:text-orange-500 hover:border-orange-500 
             transition ease-in duration-200 hover:scale-105">
-                <div className="text-center text-2xl  mt-10 mb-10">Graphical Method</div>
+                <div className="text-center text-2xl m-10">Graphical Method</div>
         </Link>
         <Link to="/Root_of_Equation/Bisection" className="s:w-1/4 md:w-1/4 lg:w-1/4 s:h-1/4 md:h-1/3 lg:h-1/4 border border-slate-300 
             rounded-lg m-5 no-underline text-black hover:text-orange-500 hover:border-orange-500 
             transition ease-in duration-200 hover:scale-105">
-                <div className="text-center text-2xl mt-10 mb-10">Bisection Method</div>
+                <div className="text-center text-2xl m-10">Bisection Method</div>
         </Link>
         <Link to="/Root_of_Equation/False-Position" className="s:w-1/4 md:w-1/4 lg:w-1/4 s:h-1/4 md:h-1/3 lg:h-1/4 border border-slate-300 
             rounded-lg m-5 no-underline text-black hover:text-orange-500 hover:border-orange-500 
             transition ease-in duration-200 hover:scale-105">
-                <div className="text-center text-2xl mt-10 mb-10">False-Position Method</div>
+                <div className="text-center text-2xl m-10">False-Position Method</div>
         </Link>
         <Link to="/Root_of_Equation/One-Point_Iteration" className="s:w-1/4 md:w-1/4 lg:w-1/4 s:h-1/4 md:h-1/3 lg:h-1/4 border border-slate-300 
             rounded-lg m-5 no-underline text-black hover:text-orange-500 hover:border-orange-500 
             transition ease-in duration-200 hover:scale-105">
-                <div className="text-center text-2xl mt-10 mb-10">One-Point Iteration Method</div>
+                <div className="text-center text-2xl m-10">One-Point Iteration Method</div>
         </Link>
         </div>
     </div>

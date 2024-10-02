f05e59619a7940852580c6c6f8652e710f7369df
Kritsakorn Khamwiset <95012380+KKhamwiset@users.noreply.github.com>
2024-10-03 04:03:08 +0700

    Update MathEquation.jsx

--- Numer/src/Component/Elements/MathEquation.jsx
+++ Numer/src/Component/Elements/MathEquation.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { MathJax, MathJaxContext } from 'better-react-mathjax';
 const MathEquation = ({ equation }) => {
     const config = {

3dea32bf088e31d7f46ef047287018c40ae1f6e1
Kritsakorn Khamwiset <95012380+KKhamwiset@users.noreply.github.com>
2024-10-03 04:23:19 +0700

    Update MathEquation.jsx

--- Numer/src/Component/Elements/MathEquation.jsx
+++ Numer/src/Component/Elements/MathEquation.jsx
@@ -1,21 +1,37 @@
-import React, { useEffect } from 'react';
-import { MathJax, MathJaxContext } from 'better-react-mathjax';
-const MathEquation = ({ equation }) => {
-    const config = {
-      loader: { load: ["[tex]/html"] },
-      tex: {
-        packages: { "[+]": ["html"] },
-        inlineMath: [["$", "$"]],
-        displayMath: [["\\(", "\\)"]]
-      }
-    }
-  return (
-    <MathJaxContext config={config} version={3}>
-      <div>
-        <MathJax inline>{equation}</MathJax>
-      </div>
-    </MathJaxContext>
-  );
-};
+import React, { useEffect, useRef } from 'react'
+import { MathJax, MathJaxContext } from 'better-react-mathjax'
 
-export default MathEquation;
+const config = {
+    loader: { load: ['input/asciimath'] },
+    asciimath: {
+        displaystyle: true,
+        delimiters: [
+            ['$', '$'],
+        ]
+    }
+}
+const PreviewMaths = (props) => {
+    const { sentence, show } = props
+    const mathJaxRef = useRef()
+
+    useEffect(() => {
+        if (mathJaxRef?.current) {
+            mathJaxRef?.current?.typeset()
+        }
+    }, [sentence])
+   
+    return (
+        <>
+            {!show && <center><h3>Preview</h3></center>}
+            {sentence && <MathJaxContext config={config}>
+                <pre style={{ marginBottom: 0, overflow: 'unset' }}>
+                    <MathJax ref={mathJaxRef} style={{ whiteSpace: 'pre-wrap',overflowY: 'clip' }} inline dynamic>
+                        {sentence || ' '}
+                        </MathJax>
+                    </pre>
+                </MathJaxContext>}
+        </>
+    )
+}
+
+export default PreviewMaths

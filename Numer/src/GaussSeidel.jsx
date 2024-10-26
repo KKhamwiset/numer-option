2d1a90f2ca5749f2a45f2bbc77c141a536583202
Kritsakorn Khamwiset <kritsakorn224@gmail.com>
2024-10-27 00:32:52 +0700

    Update GaussSeidel.jsx

--- Numer/src/GaussSeidel.jsx
+++ Numer/src/GaussSeidel.jsx
@@ -122,7 +122,7 @@ const GuassSeidel = () => {
     <div className="flex flex-col items-center mt-20">
       <h2 className="text-center text-5xl mb-10">Guass Seidel Method</h2>
       <div className="flex flex-col items-center mb-4 rounded-lg border-black border-2 p-10 mt-auto justify-center">
-        <div className="flex flex-col justify-center items-center gap-4">
+        <div className="flex flex-col justify-center items-left gap-4">
           <div>
             <label className="mb-1">Matrix Dimension (n × n):</label>
             <input 

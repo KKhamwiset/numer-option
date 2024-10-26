f48c57f4a1e927cb03c01ce71a24a882903f6949
Kritsakorn Khamwiset <kritsakorn224@gmail.com>
2024-10-27 01:49:11 +0700

    Update GaussJordan.jsx

--- Numer/src/GaussJordan.jsx
+++ Numer/src/GaussJordan.jsx
@@ -136,25 +136,22 @@ const GaussJordan = () => {
               />
             </div>
           </div>
-          <div>
-            <div className='flex flex-col justify-center items-center mx-2'>
-              <MathEquation equation="\\{x\\}" />
-              <MatrixInput
-                n={dimension}
-                m={1}
-                textlabel="x"
-                initialMatrix={matrixX}
-                onMatrixChange={setMatrixX}
-                disable={true}
-              />
-            </div>
+          <div className="flex flex-col justify-center items-center mx-2">
+            <MathEquation equation="\{x\}" />
+            <MatrixInput
+              n={dimension}
+              m={1}
+              textlabel="x"
+              initialMatrix={matrixX}
+              disable={true}
+            />
           </div>
           <div className="my-auto">
             <MathEquation equation="=" />
           </div>
           <div>
-            <div className='flex flex-col justify-center items-center mx-2'>
-              <MathEquation equation="\\{B\\}" />
+            <div className="flex flex-col justify-center items-center mx-2">
+              <MathEquation equation="\{B\}" />
               <MatrixInput
                 n={dimension}
                 m={1}

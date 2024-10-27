83ba490c802e7b6a84102892cb3a6ef099063b45
Kritsakorn Khamwiset <kritsakorn224@gmail.com>
2024-10-27 15:49:10 +0700

    Update NewtonDivided.jsx

--- Numer/src/NewtonDivided.jsx
+++ Numer/src/NewtonDivided.jsx
@@ -2,6 +2,31 @@ import { useState,useEffect } from "react";
 import { evaluate,derivative } from 'mathjs';
 
 const NewtonDivided = () => {
+    const [data, setData] = useState([]);
+    const [point,setPoint] = useState(0);
+    const [xValue,setXValue] = useState(0);
+    const C = [];
+    const CalculateC = () => {
+        const n = x.length;
+        for (let i = 0; i < n; i++) {
+            C[i] = y[i];
+            for (let j = 0; j < n - i - 1; j++) {
+                C[j] = (C[j + 1] - C[j]) / (x[j + i + 1] - x[j]);
+            }
+        }
+    }
+    const CalculateFX = (xTarget) => {
+        const n = C.length;
+        let result = 0;
+        for (let i = 0; i < n; i++) {
+            let term = C[i];
+            for (let j = 0; j < i; j++) {
+                term *= (xTarget - x[j]);
+            }
+            result += term;
+        }
+        return result;
+    }
     return (
     <>
         <div className="flex flex-col items-center mt-20">

bc41c60c6184649bbf34fa616457c22125f3ce2f
Kritsakorn Khamwiset <kritsakorn224@gmail.com>
2024-10-28 23:14:21 +0700

    Update _db.js

--- Numer/api/_db.js
+++ Numer/api/_db.js
@@ -26,13 +26,11 @@ export async function connectToDatabase() {
   if (cached.conn) {
     return cached.conn;
   }
-
   if (!cached.promise) {
     const opts = {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     };
-
     cached.promise = mongoose.connect(MONGODB_URI, opts);
   }
   cached.conn = await cached.promise;

a4be479a9958f8eea5a99349f489ac77f92199fe
Kritsakorn Khamwiset <kritsakorn224@gmail.com>
2024-10-30 23:58:37 +0700

    db

--- server/routes/numerRoutes.js
+++ server/routes/numerRoutes.js
@@ -3,4 +3,5 @@ const mainCalculation = require('../controller/calculation');
 // สร้าง Endpoint สำหรับการบันทึกข้อมูลที่ path
 router.post('/calculate', mainCalculation._createTable);
 router.get('/calculate', mainCalculation._getData);
+
 module.exports = router;

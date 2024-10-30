4fd1fe04b413a97c50ec78ffa4b548efb795d33a
Kritsakorn Khamwiset <kritsakorn224@gmail.com>
2024-10-30 18:17:14 +0700

    Update numerRoutes.js

--- server/routes/numerRoutes.js
+++ server/routes/numerRoutes.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const mainCalculation = require('../controller/calculation');
 
 // สร้าง Endpoint สำหรับการบันทึกข้อมูลที่ path /graphical
-router.post('/', mainCalculation._createTable);
-router.get('/', mainCalculation._getData);
+router.post('/calculate', mainCalculation._createTable);
+router.get('/calculate', mainCalculation._getData);
 module.exports = router;

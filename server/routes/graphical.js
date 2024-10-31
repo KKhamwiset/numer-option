dd718e7064b122cc1645fa7395f4f997d0e497e0
Kritsakorn Khamwiset <kritsakorn224@gmail.com>
2024-10-31 23:23:28 +0700

    test

--- server/routes/graphical.js
+++ server/routes/graphical.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const mainCalculation = require('../controller/rootofeq');
 // สร้าง Endpoint สำหรับการบันทึกข้อมูลที่ path
-router.post('/', mainCalculation._createTable);
-router.get('/', mainCalculation._getData);
+router.post('/', mainCalculation._sendBisection);
+
 
 module.exports = router;

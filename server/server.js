2a535c419210c61b04e4df9d63d5d3ecd73da75c
Kritsakorn Khamwiset <kritsakorn224@gmail.com>
2024-10-31 17:26:19 +0700

    test rountes

--- server/server.js
+++ server/server.js
@@ -48,7 +48,7 @@ app.get('/', async (req, res) => {
 });
 
 app.use('.api/calculate',numerRoutes);
-app.use('Root_of_Equation/Bisection/api', numerRoutes);
+app.use('Root_of_Equation/Bisection/api/calculate', numerRoutes);
 
 module.exports = app;
 

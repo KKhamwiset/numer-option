2ebd92a8029149e866851ba00db037e5b61fcc61
Kritsakorn Khamwiset <kritsakorn224@gmail.com>
2024-10-31 12:14:37 +0700

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,39 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://kritsakorn224:wDGzgpROazscmR13@cluster0.qftnd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+        const uri = process.env.MONGODB_URI;
+        if (!uri) {
+            throw new Error('MONGODB_URI is not defined in environment variables');
+        }
+        const options = {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000,
+            socketTimeoutMS: 45000,
+            family: 4, 
+        };
+
+        const conn = await mongoose.connect(uri, options);
+        
+        mongoose.connection.on('connected', () => {
+            console.log('MongoDB connected successfully');
         });
-        console.log('MongoDB connected successfully');
+
+        mongoose.connection.on('error', (err) => {
+            console.error('MongoDB connection error:', err);
+        });
+
+        mongoose.connection.on('disconnected', () => {
+            console.log('MongoDB disconnected');
+        });
+
+        return conn;
+
     } catch (error) {
         console.error('MongoDB connection error:', error);
-        process.exit(1);
+        throw error;
     }
 };
 

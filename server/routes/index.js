c7fa2d00388bb2f00ec77bcc75ef1999f9288048
Kritsakorn Khamwiset <kritsakorn224@gmail.com>
2024-11-01 00:07:37 +0700

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const rootofEquationApi = require('./rootofEQ')
+const table = require('./table')
 // สร้าง Endpoint สำหรับการบันทึกข้อมูลที่ path
 
 router.use('/rootofEQ', rootofEquationApi);
-
+router.use('/table', table);
 
 module.exports = router;

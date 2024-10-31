8c0be10b6f3b3e6b2315faf1fda61b7cdf20f251
Kritsakorn Khamwiset <kritsakorn224@gmail.com>
2024-10-31 19:55:25 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,6 +17,13 @@ const initDB = async () => {
     }
 };
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://numer-option-second.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use(cors({
     origin:
     [

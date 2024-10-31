086c1bf0f28a91d3bf6419bc2eaba5157eea880a
Kritsakorn Khamwiset <kritsakorn224@gmail.com>
2024-11-01 03:14:37 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -24,7 +24,6 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     initDB();
-
     next();
 });
 

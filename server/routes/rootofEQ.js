d85d68cde3c9a2e98f44523db60eae059e533f93
Kritsakorn Khamwiset <kritsakorn224@gmail.com>
2024-11-01 21:23:01 +0700

    added swagger des

--- server/routes/rootofEQ.js
+++ server/routes/rootofEQ.js
@@ -1,29 +1,36 @@
 const router = require('express').Router();
 const mainCalculation = require('../controller/rootofeq');
 
-
 /**
  * @swagger
  * /api/rootofEQ:
  *   post:
  *     summary: Add data to DB
-  *     requestBody:
+ *     description: Adds a new set of data to the database.
+ *     tags:
+ *       - Root Equations
+ *     requestBody:
  *       required: true
  *       content:
  *         application/json:
  *           schema:
- *             type: object 
+ *             type: object
  *             properties:
  *               x_start:
  *                 type: string
+ *                 description: Starting value of x.
  *               x_end:
  *                 type: string
+ *                 description: Ending value of x.
  *               equation:
  *                 type: string
+ *                 description: The mathematical equation.
  *               answer:
  *                 type: string
+ *                 description: The calculated answer.
  *               subtype:
  *                 type: string
+ *                 description: Subtype of the equation.
  *             required:
  *               - x_start
  *               - x_end
@@ -32,25 +39,30 @@ const mainCalculation = require('../controller/rootofeq');
  *               - subtype
  *     responses: 
  *       200:
- *         description: A list of items
+ *         description: Successfully added the item.
  */
 router.post('/', mainCalculation._sendRootEQ);
-
 
 /**
  * @swagger
  * /api/rootofEQ/{id}:
  *   delete:
  *     summary: Delete specific item
+ *     description: Deletes an item by its ID.
+ *     tags:
+ *       - Root Equations
  *     parameters:
  *       - in: path
  *         name: id
  *         required: true
  *         schema:
  *           type: string
+ *         description: ID of the item to delete.
  *     responses: 
  *       200:
- *         description: A list of items
+ *         description: Successfully deleted the item.
+ *       404:
+ *         description: Item not found.
  */
 router.delete('/:id', mainCalculation._deleteCalculation);
 
